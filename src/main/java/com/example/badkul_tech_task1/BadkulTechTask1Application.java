572a5753f0bb84e275a7907c2e7c9c961309e14e
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-11 10:12:32 +0530

    initial setup

---
+++ src/main/java/com/example/badkul_tech_task1/BadkulTechTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.badkul_tech_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadkulTechTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadkulTechTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/badkul_tech_task1/BadkulTechTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.badkul_tech_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadkulTechTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
