01396d906e62b440e7480e60f234bc71aafb09dd
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-12 08:47:48 +0530

    gettrip by id feature.

--- src/main/java/com/example/badkul_tech_task1/controller/TripController.java
+++ src/main/java/com/example/badkul_tech_task1/controller/TripController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/")
@@ -39,14 +40,30 @@ public class TripController {
 
 
 
-    //endpoint for getting a trip by Id
+    //endpoint for getting all trips
+    @GetMapping("trips")
+    public ResponseEntity<ApiResponse<List<Trip>>> getAllTrip(@PathVariable Long id){
+        List<Trip> fetchedTrips=tripService.getAllTrip();
+
+        ApiResponse<List<Trip>> response=new ApiResponse<>(
+                fetchedTrips,
+                "trips fetched successfully.",
+                HttpStatus.OK.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+
+     //endpoint for getting a trip by id
     @GetMapping("trip/{id}")
-    public ResponseEntity<ApiResponse<Trip>> addTrip(@PathVariable Long id){
-        Trip addedTrip=tripService.addTrip(trip);
+    public ResponseEntity<ApiResponse<Trip>> getTripById(@PathVariable Long id){
+        Trip fetchedTrip=tripService.getTripById(id);
 
         ApiResponse<Trip> response=new ApiResponse<>(
-                addedTrip,
-                "trip successfully added.",
+                fetchedTrip,
+                "trip fetched successfully.",
                 HttpStatus.OK.value(),
                 LocalDateTime.now()
         );
