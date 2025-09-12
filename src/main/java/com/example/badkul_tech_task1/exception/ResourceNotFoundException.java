689a7b7da4a459579e0b9eaf3d49c4abfacc8503
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-12 08:34:16 +0530

    adding comments for documenation.

---
+++ src/main/java/com/example/badkul_tech_task1/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.badkul_tech_task1.exception;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
