a6c0a6e97675467092cf047696956e98880eda7c
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-14 11:55:37 +0530

    repository method.

--- src/main/java/com/example/badkul_tech_task1/repository/TripRepository.java
+++ src/main/java/com/example/badkul_tech_task1/repository/TripRepository.java
@@ -1,7 +1,10 @@
 package com.example.badkul_tech_task1.repository;
 
 import com.example.badkul_tech_task1.model.Trip;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TripRepository extends JpaRepository<Trip,Long> {
+public interface TripRepository extends JpaRepository<Trip, Long> {
+    Page<Trip> findByDestinationContainingIgnoreCase(String destination, Pageable pageable);
 }
