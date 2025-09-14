883d7e77bddaa125535d0854bd0c0148dfabe18f
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-14 11:56:30 +0530

    service method.

--- src/main/java/com/example/badkul_tech_task1/service/TripService.java
+++ src/main/java/com/example/badkul_tech_task1/service/TripService.java
@@ -5,16 +5,19 @@ import com.example.badkul_tech_task1.dtos.TripUpdateDTO;
 import com.example.badkul_tech_task1.model.Trip;
 import org.springframework.data.domain.Page;
 
+
 public interface TripService {
 
-  Trip addTrip(TripRequestDTO dto);
+    Trip addTrip(TripRequestDTO dto);
 
-  Page<Trip> getAllTrip(int page, int size, String destination, String direction);
+    Page<Trip> getAllTrip(int page, int size, String sortBy, String direction);
 
-  Trip getTripById(Long id);
+    Trip getTripById(Long id);
 
-  Trip updateTripById(Long id, TripUpdateDTO dto);
+    Trip updateTripById(Long id, TripUpdateDTO dto);
 
-  void deleteTripById(Long id);
+    void deleteTripById(Long id);
+
+    Page<Trip> searchTripByDestination(String destination, int page, int size, String sortBy, String direction);
 
 }
